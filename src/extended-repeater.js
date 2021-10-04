b994caa84118c4ab3b8d65ad22faea00f35aa8a9
Ann2206 <53495105+Ann2206@users.noreply.github.com>
2021-10-05 02:57:55 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,34 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
+
 export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let stroka = '';
+  let result = '';
+  if (typeof options.additionRepeatTimes == 'undefined' || options.additionRepeatTimes == 'undefined'){
+    stroka = options.addition;
+  }else{
+  for (let i=0;i<options.additionRepeatTimes-1;i++){
+    if (typeof options.additionSeparator == 'undefined' || options.additionSeparator == 'undefined'){
+      options.additionSeparator = '|';
+    }
+    let stroka1 = options.addition + options.additionSeparator;
+    stroka+= stroka1;
+  }
+  stroka += options.addition;
+  }
+  if (typeof options.repeatTimes == 'undefined' || options.repeatTimes == 'undefined'){
+    result = str;
+  }else{
+  for (let j=0;j<options.repeatTimes-1;j++){
+    if (typeof options.separator == 'undefined' || options.separator == 'undefined'){
+      options.separator = '+';
+    }
+    let res = str + stroka + options.separator;
+    result += res;
+  }
+  result += (str + stroka);
+  }
+  return result;
+};
+
 }
