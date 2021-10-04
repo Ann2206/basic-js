1ae976526cb0c43c316a2f85de8b00e4113ac9b4
Ann2206 <53495105+Ann2206@users.noreply.github.com>
2021-10-05 02:50:08 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,17 +1,1 @@
 
-
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample( sampleActivity ) {
-  if (typeof sampleActivity != 'String' && typeof sampleActivity != 'Number'){
-    return false;
-  }
-  if (Number(sampleActivity) == 'undefined'){
-    return false;
-  }
-  let k = Math.log(2)/HALF_LIFE_PERIOD;
-  let chs = MODERN_ACTIVITY/sampleActivity;
-  let t = Math.log(chs)/k;
-  return t;
-};
